3b5c77c505e04415b1ed3b2116f6e3daa412e8d4
Davide <zago-7@live.it>
2019-04-03 17:27:39 +0200

    word count 1 modificato e prima parte word count 2

--- Homework2/src/G11HM2.java
+++ Homework2/src/G11HM2.java
@@ -1,39 +1,78 @@
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.lang.String;
+import scala.Tuple2;
+
 
 public class G11HM2 {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         if (args.length == 0) {
             throw new IllegalArgumentException("Expecting the file name on the command line");
         }
-
-        // Read a list of numbers from the program options
-        ArrayList<Double> lNumbers = new ArrayList<>();
-        Scanner s =  new Scanner(new File(args[0]));
-        while (s.hasNext()){
-            lNumbers.add(Double.parseDouble(s.next()));
-        }
-        s.close();
-
         // Setup Spark
         SparkConf conf = new SparkConf(true)
                 .setAppName("HW2")
                 .setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        // Create a parallel collection
-        JavaRDD<Double> dNumbers = sc.parallelize(lNumbers);
+        JavaRDD<String> docs = sc.textFile(args[0]);
+        int k = (int)Math.sqrt(Integer.parseInt(args[1]));
+        System.out.println(k);
 
-        double sumOfSquares = dNumbers.map((x) -> x*x).reduce((x, y) -> x + y);
-        System.out.println("The sum of squares is " + sumOfSquares);
+        JavaPairRDD<String, Long> wordcountpairs = docs
+                // Map phase
+                .flatMapToPair((document) -> {
+                    String[] tokens = document.split(" ");
+                    HashMap<String, Long> counts = new HashMap<>();
+                    ArrayList<Tuple2<String, Long>> pairs = new ArrayList<>();
+                    for (String token : tokens) {
+                        counts.put(token, 1L + counts.getOrDefault(token, 0L));
+                    }
+                    for (Map.Entry<String, Long> e : counts.entrySet()) {
+                        pairs.add(new Tuple2<>(e.getKey(), e.getValue()));
+                    }
+                    return pairs.iterator();
+                })
+                // Reduce phase
+                .reduceByKey((x,y) -> x+y);
+
+        JavaPairRDD<String, Long> wordcountpairs2  = sc.textFile(args[0])
+                // Map - 1
+                .flatMapToPair((document) -> {
+                    String[] tokens = document.split(" ");
+                    HashMap<String, Long> counts = new HashMap<>();
+                    ArrayList<Tuple2<String, Long>> pairs = new ArrayList<>();
+                    for (String token : tokens) {
+                        counts.put(token, 1L + counts.getOrDefault(token, 0L));
+                    }
+                    for (Map.Entry<String, Long> e : counts.entrySet()) {
+                        pairs.add(new Tuple2<>(e.getKey(), e.getValue()));
+                    }
+                    return pairs.iterator();
+                }).groupBy(x -> (int)Math.random()*k)
+                // Reduce - 1
+                .reduceByKey(x -> {
+
+                })
+
+
+        wordcountpairs.foreach(data -> {
+            System.out.println("Word: " + data._1() + "---- Count: "+ data._2());
+        });
+
 
     }
+
 
 }
