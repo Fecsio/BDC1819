4ce193934324fd09389ccfa86f9bd18901b64846
Davide <zago-7@live.it>
2019-04-06 16:51:00 +0200

    fatta prima variante del word count 2

--- Homework2/src/G11HM2.java
+++ Homework2/src/G11HM2.java
@@ -6,10 +6,7 @@ import org.apache.spark.api.java.JavaSparkContext;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 import java.lang.String;
 import scala.Tuple2;
 
@@ -27,8 +24,11 @@ public class G11HM2 {
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         JavaRDD<String> docs = sc.textFile(args[0]);
-        int k = (int)Math.sqrt(Integer.parseInt(args[1]));
-        System.out.println(k);
+        // Parameter k given by cmd line
+        int k = Integer.parseInt(args[1]);
+
+        /* 2 - code for the Improved Word count 1 algorithm described in class the using
+         reduceByKey method to compute the final counts */
 
         JavaPairRDD<String, Long> wordcountpairs = docs
                 // Map phase
@@ -46,8 +46,11 @@ public class G11HM2 {
                 })
                 // Reduce phase
                 .reduceByKey((x,y) -> x+y);
+        
+        /*2 - code fore a variant of the algorithm presented in class where random keys take K possible values,
+        where K is the value given in input*/
 
-        JavaPairRDD<String, Long> wordcountpairs2  = sc.textFile(args[0])
+        JavaPairRDD<String, Long> wordcountpairs2 = docs
                 // Map - 1
                 .flatMapToPair((document) -> {
                     String[] tokens = document.split(" ");
@@ -60,17 +63,32 @@ public class G11HM2 {
                         pairs.add(new Tuple2<>(e.getKey(), e.getValue()));
                     }
                     return pairs.iterator();
-                }).groupBy(x -> (int)Math.random()*k)
+                }).groupBy(x -> new Long((int)(Math.random()*k)))
                 // Reduce - 1
-                .reduceByKey(x -> {
-
+                .flatMapToPair(x -> {
+                    Iterator<Tuple2<String, Long>> iter = x._2().iterator();
+                    HashMap<String, Long> count = new HashMap<>();
+                    ArrayList<Tuple2<String, Long>> pairs = new ArrayList<>();
+                    while(iter.hasNext()) {
+                        Tuple2 t = (Tuple2)iter.next();
+                        String key = (String) t._1();
+                        Long value = (Long) t._2();
+                        count.put(key, value + count.getOrDefault(key,0L));
+                    }
+                    for(Map.Entry<String, Long> e : count.entrySet()){
+                        pairs.add(new Tuple2<>(e.getKey(), e.getValue()));
+                    }
+                    return pairs.iterator();
                 })
+                // map - 2: Identity
+                // reduce - 2
+                .reduceByKey((x,y) -> x+y);
 
-
-        wordcountpairs.foreach(data -> {
+        wordcountpairs2.foreach(data -> {
             System.out.println("Word: " + data._1() + "---- Count: "+ data._2());
         });
 
+        //System.in.read();
 
     }
 
